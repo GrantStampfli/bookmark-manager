84e251ade2d72c4e0a71ed73460cb6ded1301c5d
Grant Stampfli <mail@gstampfli.com>
2014-12-18 12:32:36 -0800

    added a model and filter to bookmarks.

--- app/main-app.js
+++ app/main-app.js
@@ -5,20 +5,34 @@ angular.module('Manager', [
         $scope.categories = [
             {"id": 0, "name": "JavaScript"},
             {"id": 1, "name": "Themes"},
-            {"id": 2, "name": "Interesting"},
-            {"id": 3, "name": "Keep"}
+            {"id": 2, "name": "Hosting"},
+            {"id": 3, "name": "Programing"}
         ];
+
         $scope.bookmarks = [
             {"id": 0, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 1, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 2, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 3, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 4, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 5, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 6, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 7, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
-            {"id": 8, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"}
+            {"id": 1, "title": "JavaScript PCS", "url": "http://portlandcodeschool.github.io/jsi", "category": "JavaScript"},
+            {"id": 2, "title": "Bootstrap Themes", "url": "http://prepbootstrap.com", "category": "Themes"},
+            {"id": 3, "title": "GoDaddy", "url": "http://godaddy.com", "category": "Hosting"},
+            {"id": 4, "title": "DigitalOcean", "url": "http://digitalocean.com", "category": "Hosting"},
+            {"id": 5, "title": "Plural Site", "url": "http://pluralstie.com", "category": "Programing"},
+            {"id": 6, "title": "Google Developers", "url": "http://developers.google.com", "category": "Programing"},
+            {"id": 7, "title": "Fuel PHP", "url": "http://fuelphp.com", "category": "Programing"},
+            {"id": 8, "title": "To Do MVC", "url": "http://todomvc.com", "category": "Programing"}
 
         ];
+
+        $scope.currentCategory = null;
+
+        function setCurrentCategory(category) {
+            $scope.currentCategory = category;
+        }
+
+        function isCurrentCategory(category) {
+            return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+        }
+
+        $scope.setCurrentCategory = setCurrentCategory;
+        $scope.isCurrentCategory = isCurrentCategory;
 })
 ;
