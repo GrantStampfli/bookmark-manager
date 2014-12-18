6d01bfecde50f742e48e7d2b4bcb10fd5ea63000
Grant Stampfli <mail@gstampfli.com>
2014-12-18 12:10:05 -0800

    intergrated model

--- app/main-app.js
+++ app/main-app.js
@@ -2,6 +2,23 @@ angular.module('Manager', [
 
 ])
 .controller('MainCtrl', function($scope) {
-   $scope.hello = 'world'
+        $scope.categories = [
+            {"id": 0, "name": "JavaScript"},
+            {"id": 1, "name": "Themes"},
+            {"id": 2, "name": "Interesting"},
+            {"id": 3, "name": "Keep"}
+        ];
+        $scope.bookmarks = [
+            {"id": 0, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 1, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 2, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 3, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 4, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 5, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 6, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 7, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"},
+            {"id": 8, "title": "angularjs", "url": "http://angularjs.org", "category": "JavaScript"}
+
+        ];
 })
 ;
